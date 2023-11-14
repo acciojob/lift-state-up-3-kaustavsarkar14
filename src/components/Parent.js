00ef308580d033c9bec9be249811b511321925b1
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-14 10:34:25 +0530

    first commit

---
+++ src/components/Parent.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import Child1 from './Child1'
+import Child2 from './Child2'
+
+const Parent = () => {
+    const [selectedOption, setSelectedOption]= useState('')
+  return (
+    <div className='parent'  style={{backgroundColor:"green"}} >
+        <h1>Parent Component</h1>
+        <Child1 setSelectedOption={setSelectedOption} />
+        <Child2 setSelectedOption={setSelectedOption} />
+        <p>Selected Option: {selectedOption}</p>
+    </div>
+  )
+}
+
+export default Parent
